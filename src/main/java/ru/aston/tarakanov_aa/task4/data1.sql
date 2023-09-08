834b92147bf0e05c8333b9104315056d14dd975b
Tarakanov_AA <stev27@yandex.ru>
2023-09-08 15:36:38 +0300

    create task4

---
+++ src/main/java/ru/aston/tarakanov_aa/task4/data1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE USER(
+ID INT auto_increment PRIMARY KEY,
+NAME VARCHAR(255));
+
+insert into test(name) values('hello');
+insert into test(name) values('world111');
---
+++ src/main/java/ru/aston/tarakanov_aa/task4/orders.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ORDER(
+id INT auto_increment PRIMARY KEY,
+product VARCHAR(255),
+weight REAL,
+price REAL);
+
+INSERT INTO USER(name) values('hello');
